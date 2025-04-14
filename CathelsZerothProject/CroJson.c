c12ef96b34db0748bf2fdb55a143ca716669d2de
Cathel Robertson <cathelrobertson@gmail.com>
2025-04-14 20:04:31 +0100

    Working on a separate tokenizer method which will produce a token chain. THe methods from the previous approach will be re-purposed to use this chain and make the code easier to handle

--- CathelsZerothProject/CroJson.c
+++ CathelsZerothProject/CroJson.c
@@ -20,14 +20,17 @@ static Error gl_error;
 
 typedef struct Token
 {
-    int index;
-    char* value;
+    char* content;
+    char value;
+
 } Token;
 
-typedef struct MemPool
+typedef struct StringPool
 {
-    void* pool;
-};
+    char* stringPool;
+    char* nextBlock;
+    int size;
+} StringPool;
 /*==================================================================================================================================================*/
 
 /*Macro definition*/
@@ -44,21 +47,52 @@ typedef struct MemPool
 #define read_success (1<<1)
 
 
-
-void TokenizeObject(char* jsonString, int cursor)
+//Top level - Interface to recursive methods
+TreeNode* GetJsonTree(char* jsonString)
 {
-    int length = strlen(jsonString);
-    for (int i = 0; i < length; i++)
+    bool isSuccess = false;
+    /*Fine to use a local instance of JsonBuffer here as we're not reutrning it, its cleanup gets handled when the method returns*/
+    JsonBuffer buffer = { jsonString, 0,strlen(jsonString),0 };
+    JsonBuffer* bPtr = &buffer;
+
+    Token* pool = TokenizeJson(bPtr);
+
+    //Everything else works on the token chain
+    TreeNode* root = malloc(sizeof(TreeNode));
+    if (root == NULL) return NULL;
+    root->child = NULL;
+    root->next = NULL;
+    root->name = malloc(5 * sizeof(char));
+    strcpy(root->name, "root");
+    SkipWhiteSpace(bPtr, false);
+    if ((buffer_at_cursor(bPtr)) == '{')
     {
-        switch (jsonString[i])
+        isSuccess = ParseObject(bPtr, root);
+    }
+    if (isSuccess) {
+        return root;
+    }
+    else {
+        PrintError();
+        return NULL;
+    }
+}
+
+bool TokenizeJson(JsonBuffer* bPtr)
+{
+    StringPool* pool = malloc(sizeof(StringPool));
+    pool->stringPool = malloc(bPtr->length * sizeof(char));
+    Token* tokens = malloc(bPtr->length*sizeof(Token));
+    int currToken = 0;
+    for (int i = 0; i < bPtr->length; i++)
+    {
+        switch (bPtr->jsonString[i])
         {
         case '{':
-            //Check Valid
-            // //ParseObject
-            //add token
+            tokens + currToken = NewCharToken();
             break;
         case '}':
-            //Check Valid
+
             //add token
             break;
         case '"':
@@ -66,6 +100,7 @@ void TokenizeObject(char* jsonString, int cursor)
             //Add content as token
             break;
         case ':':
+            //Skipwhitespace -> read content
             //Do read content
             //add token
             break;
@@ -79,15 +114,209 @@ void TokenizeObject(char* jsonString, int cursor)
             //addtoken
             break;
         default:
-            //readcontent
             continue;
         }
     }
 }
 //Check valid could be something like a tree for each character.
 
+
+void ParseObject(char* jsonString)
+{
+    Token* pool = InitializePool(strlen(jsonString));
+    TokenizeObject(pool, jsonString);
+    if (pool == NULL)
+    {
+
+    }
+    //If theres sub objects handle that here
+    // 
+    //So here we're dealing with a string if tokens
+    while (/*Theres still tokens*/)
+    {
+
+    }
+}
+
+void ParseList()
+{
+
+}
+
+
 void CheckValid()
 {
     
 }
 
+
+
+//"":, - for a non string, we immediately return - fine, for a non string...?
+/*Method to read text - works for strings and non strings depending on checking method passed in*/
+char* ReadContent(JsonBuffer* bPtr, bool isString)
+{
+    char* string = malloc(bPtr->length - bPtr->cursor);
+    if (string == NULL)
+    {
+        SetError("Memory Allocation failure", name_of(ReadContent), bPtr->cursor);
+        return NULL;
+    }
+    int index = 0;
+    Byte byte;
+    byte.flags = 0;
+    void (*CheckChar)(JsonBuffer*, char*, int*, Byte*);
+
+    if (isString && buffer_can_advance(bPtr))
+    {
+        CheckChar = &CheckCharString;
+        if ((buffer_at_offset(bPtr, 1)) != ('\"'))
+        {
+            SetError("Syntax Error, missing open quote", name_of(ReadContent), bPtr->cursor);
+            free(string);
+            return NULL;
+        }
+        else
+        {
+            buffer_advance(bPtr);
+        }
+    }
+    else
+    {
+        CheckChar = &CheckCharNonString;
+    }
+    while (buffer_can_advance(bPtr))
+    {
+        buffer_advance(bPtr); /*Need to advance before checking, otherwise we lose the last character in the string*/
+        CheckChar(bPtr, string, &index, &byte);
+        if (byte.flags & read_finished) break;
+    }
+
+    if (byte.flags & read_success)
+    {
+        *(string + index) = '\0';
+        char* holder = string;
+        string = realloc(string, (index + 1) * sizeof(char));
+        //printf("%s\n", string);
+        if (string == NULL)
+        {
+            SetError("Memory Allocation failure", name_of(ReadContent), bPtr->cursor);
+            free(holder);
+            return NULL;
+        }
+        else return string;
+    }
+    else
+    {
+        SetError("Failed to read content", name_of(ReadContent), bPtr->cursor);
+        free(string);
+        return NULL;
+    }
+}
+
+
+/*This is one possible strategy for reading content*/
+/*Not super happy with the nested switch case here - could have been done neater...*/
+void CheckCharString(JsonBuffer* bPtr, char* content, int* indexPtr, Byte* byte)
+{
+    char currChar = buffer_at_cursor(bPtr);
+    switch (currChar)
+    {
+    case'\"':
+        if (buffer_can_advance(bPtr))
+        {
+            //printf("bef: %c\n", buffer_at_cursor(bPtr));
+            buffer_advance(bPtr);
+            //printf("aft: %c\n", buffer_at_cursor(bPtr));
+        }
+        byte->flags = byte->flags | read_finished;
+        byte->flags = byte->flags | read_success;
+        break;
+    case '\\':
+        if (buffer_can_advance(bPtr))
+        {
+            buffer_advance(bPtr);
+            currChar = buffer_at_cursor(bPtr);
+            switch (currChar)
+            {
+            case '\"':
+                AddCharToContent('\"', content, indexPtr);
+                break;
+            case '\\':
+                AddCharToContent('\\', content, indexPtr);
+                break;
+            case 'b':
+                AddCharToContent('\b', content, indexPtr);
+                break;
+            case 'f':
+                AddCharToContent('\f', content, indexPtr);
+                break;
+            case 'n':
+                AddCharToContent('\n', content, indexPtr);
+                break;
+            case 'r':
+                AddCharToContent('\r', content, indexPtr);
+                break;
+            case 't':
+                AddCharToContent('\t', content, indexPtr);
+                break;
+            default:
+                SetError("Syntax Error, invalid escape sequence", name_of(CheckCharString), bPtr->cursor);
+                byte->flags = byte->flags | read_finished;
+                break;
+            }
+        }
+        break;
+    default:
+        AddCharToContent(currChar, content, indexPtr);
+        break;
+    }
+    return;
+}
+
+
+
+/*We don't worry about whether the char is valid here, we're just looking to read content*/
+void CheckCharNonString(JsonBuffer* bPtr, char* content, int* indexPtr, Byte* byte)
+{
+    char currChar = buffer_at_cursor(bPtr);
+    if (currChar == ' ' || currChar == ',' || currChar == '}' || currChar == ']')
+    {
+        byte->flags = byte->flags | read_finished;
+        byte->flags = byte->flags | read_success;
+    }
+    else
+    {
+        AddCharToContent(currChar, content, indexPtr);
+    }
+    return;
+}
+
+
+
+void AddCharToContent(char currChar, char* string, int* indexPtr)
+{
+    *(string + *indexPtr) = currChar;
+    *indexPtr += 1;
+    return;
+}
+
+
+void PrintError()
+{
+    printf("\n!!!->Error: %s || Character: %d\n   ->Call Stack: %s\n", gl_error.errorMessage, gl_error.charPos, gl_error.methodName);
+}
+
+
+void SetError(char* errorMessage, char* methodName, int charPos)
+{
+    strcpy(gl_error.errorMessage, errorMessage);
+    strcpy(gl_error.methodName, methodName);
+    gl_error.charPos = charPos;
+
+}
+
+void AddErrorCallStack(char* methodName)
+{
+    strcat(gl_error.methodName, "<-");
+    strcat(gl_error.methodName, methodName);
+}
